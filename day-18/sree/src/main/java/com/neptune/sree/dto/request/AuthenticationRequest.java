7bea8f9545cf2acc034c3d5281b4d2d2bad370f2
Sreejanani17 <120357306+Sreejanani17@users.noreply.github.com>
2023-10-16 11:15:28 +0530

    Add files via upload

---
+++ day-18/sree/src/main/java/com/neptune/sree/dto/request/AuthenticationRequest.java
@@ -0,0 +1,16 @@
+package com.neptune.sree.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest {
+
+	private String email;
+	private String pword;
+}
